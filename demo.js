3cc3770be15dc3031e99ee1751540404856ea363
aksoni27 <42676217+aksoni27@users.noreply.github.com>
2020-08-09 11:26:18 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,48 +1,49 @@
 var http = require('http');
 http.createServer(function (req, res) {
 
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, { 'Content-Type': 'text/html' });
   var fs = require('fs');
-//const { json } = require("express");
-var file = 'api/routes/tales.txt';
-fs.readFile(file, 'utf8', function (err, data) {
+  //const { json } = require("express");
+  var file = 'tales.txt';
+  fs.readFile(file, 'utf8', function (err, data) {
 
-  if (err) throw err;
+    if (err) throw err;
 
-  var wordsArray = splitByWords(data);
-  var wordsMap = createWordMap(wordsArray);
-
-
-  var finalWordsArray = sortByCount(wordsMap);
-   var newArray=[];
-   let n=10;
-   
-      for(i=0;i<n;i++){
-         newArray.push(finalWordsArray[i]);
-
-      }
-  res.write(JSON.stringify({newArray}));
-  res.end();
-      //console.log(newArray)
-     
-});
+    var wordsArray = splitByWords(data);
+    var wordsMap = createWordMap(wordsArray);
 
 
-  
+    var finalWordsArray = sortByCount(wordsMap);
+    var newArray = [];
+    let n = 10;
+
+    for (i = 0; i < n; i++) {
+      newArray.push(finalWordsArray[i]);
+
+    }
+    console.log(newArray)
+    res.write(JSON.stringify({ newArray }));
+    res.end();
+    
+
+  });
+
+
+
 }).listen(8080);
 
 
 
 
 
-function splitByWords (text) {
+function splitByWords(text) {
   // split string by spaces (including spaces, tabs, and newlines)
   var wordsArray = text.split(/\s+/);
   return wordsArray;
 }
 
 
-function createWordMap (wordsArray) {
+function createWordMap(wordsArray) {
 
   // create map for word counts
   var wordsMap = {};
@@ -66,18 +67,18 @@ function createWordMap (wordsArray) {
 }
 
 
-function sortByCount (wordsMap) {
+function sortByCount(wordsMap) {
 
   // sort by count in descending order
   var finalWordsArray = [];
-  finalWordsArray = Object.keys(wordsMap).map(function(key) {
+  finalWordsArray = Object.keys(wordsMap).map(function (key) {
     return {
       name: key,
       total: wordsMap[key]
     };
   });
 
-  finalWordsArray.sort(function(a, b) {
+  finalWordsArray.sort(function (a, b) {
     return b.total - a.total;
   });
 
